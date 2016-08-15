7593163a3fe87fa534a8e625e72f5fca09f57247
Keakone <keanucarnevale@gmail.com>
2016-08-15 15:33:03 +1200

    hid textarea under 980px, made face center on mobile

--- js/init.js
+++ js/init.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 $('textarea').val('')
 $('textarea').delay(1000).typetype(
 'Hey friend 🙋 My name is keanu carnevale and welcome to my portfolio!',  {
-    e: 0.04, // error rate. (use e=0 for perfect typing)
+    e: 0, // error rate. (use e=0 for perfect typing)
     t: 70, // interval between keypresses
     keypress: function (){
       // called after every keypress (this may be an erroneous keypress!)
