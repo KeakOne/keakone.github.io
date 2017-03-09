b94e7d0c0d9690a9e19227aecb3291943538c74d
Keakone <keanucarnevale@gmail.com>
2017-03-09 15:06:28 +1300

    removed unneccesary files

--- js/init.js
+++ js/init.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 $('textarea').val('')
 $('textarea').delay(1000).typetype(
-'Hey friend 🙋 My name is keanu carnevale and welcome to my portfolio!',  {
+'Welcome to Carnevale design \n Creating better websites for less',  {
     e: 0, // error rate. (use e=0 for perfect typing)
     t: 70, // interval between keypresses
     keypress: function (){
